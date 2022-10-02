88f94b92294f934f4d1b9c75f359aaf7aa22aa75
DELL <DELL@DESKTOP-LILEBQG>
2022-10-02 21:36:42 +0530

    me comiited no

---
+++ src/main/java/harshatejaspackag/Sample.java
@@ -0,0 +1,17 @@
+package harshatejaspackag;
+
+import org.testng.annotations.Test;
+
+public class Sample 
+{
+@Test(groups="smoke")
+public void test()
+{
+	System.out.println("iam practise");
+}
+@Test(groups="regression")
+public void test2()
+{
+	System.out.println("iam practise2");
+}
+}
